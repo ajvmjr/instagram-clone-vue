56ce3ea8100f5b40651ccf2372bcd02d372e5960
Alex Moreira <alex.moreira@server.signainfo.com.br>
2020-10-25 22:41:50 -0300

    feat: login

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-footer class="d-flex align-center justify-space-evenly">
+      <div class="d-flex justify-center links">
+        <a href="#" class="anchor">sobre</a>
+        <a href="#" class="anchor">ajuda</a>
+        <a href="#" class="anchor">imprensa</a>
+        <a href="#" class="anchor">api</a>
+        <a href="#" class="anchor">carreiras</a>
+        <a href="#" class="anchor">privacidade</a>
+        <a href="#" class="anchor">termos</a>
+        <a href="#" class="anchor">localizações</a>
+        <a href="#" class="anchor">contas mais relevantes</a>
+        <a href="#" class="anchor">hashtags</a>
+        <a href="#" class="anchor">idioma</a>
+      </div>
+      <span>© 2020 instagram do facebook</span>
+    </v-footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.v-footer {
+  height: 80px;
+  width: 100%;
+  .links {
+    width: 80%;
+    .anchor {
+      color: #00376b;
+      font-size: 12px;
+      font-weight: 500;
+      padding: 0 10px;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+  }
+
+  span {
+    color: #8e8e8e;
+    font-size: 12px;
+    text-transform: uppercase;
+  }
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,182 @@
+<template>
+  <div style="background: #FAFAFA">
+    <v-container class="d-flex align-center justify-center v-container">
+      <section>
+        <img src="@/assets/left-img-login.png" alt="" />
+      </section>
+      <section class="v-container__cards">
+        <v-card
+          class="d-flex flex-column align-center ml-5 v-container__cards__login"
+          outlined
+          tile
+        >
+          <v-card-title class="mb-8 v-container__cards__login__title"
+            >Instagram</v-card-title
+          >
+
+          <v-form>
+            <div>
+              <v-text-field
+                background-color="#fafafa"
+                type="email"
+                filled
+                dense
+                outlined
+                label="Telefone, nome de usuário ou email"
+                v-model="email"
+              ></v-text-field>
+              <v-text-field
+                background-color="#fafafa"
+                filled
+                dense
+                outlined
+                label="Senha"
+                type="password"
+                v-model="password"
+                :append-icon="passwordWasEntered ? 'visibility' : ''"
+                @input="hasPassword()"
+              >
+              </v-text-field>
+            </div>
+            <v-btn color="#0095f6" class="mt-2" elevation="1" block
+              >Entrar</v-btn
+            >
+          </v-form>
+          <h3>ou</h3>
+          <div class="d-flex">
+            <v-icon class="mr-2 mt-4" color="#385185">facebook</v-icon>
+            <h4>Entrar com facebook</h4>
+          </div>
+          <a>Esqueceu a senha?</a>
+        </v-card>
+        <v-card
+          class="d-flex align-center justify-center mt-5 ml-5 v-container__cards__register"
+          outlined
+          tile
+        >
+          <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+        </v-card>
+      </section>
+    </v-container>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Footer,
+  },
+
+  data: () => ({
+    email: "",
+    password: "",
+    passwordWasEntered: false,
+  }),
+
+  computed: {
+    hasPassword() {
+      this.password.length > 0
+        ? (this.passwordWasEntered = true)
+        : (this.passwordWasEntered = false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-container {
+  background: #fafafa;
+  height: 100vh;
+  width: 100%;
+  &__cards {
+    width: 40%;
+
+    &__login {
+      height: 60%;
+      background: #fff;
+      width: 75%;
+      &__title {
+        color: #262626;
+        font-size: 40px;
+        font-family: "Pacifico", cursive;
+      }
+      .v-form {
+        width: 85%;
+        div {
+          .v-text-field {
+            font-size: 12px;
+          }
+        }
+        .v-btn {
+          text-transform: none;
+          color: white;
+        }
+      }
+
+      h3 {
+        color: #8e8e8e;
+        font-size: 18px;
+        font-weight: normal;
+        margin-top: 20px;
+        text-align: center;
+        width: 72%;
+        &:before,
+        &:after {
+          background-color: #8e8e8e;
+          content: "";
+          display: inline-block;
+          height: 1px;
+          position: relative;
+          vertical-align: middle;
+          width: 50%;
+        }
+
+        &:before {
+          right: 0.5em;
+          margin-left: -50%;
+        }
+
+        &:after {
+          left: 0.5em;
+          margin-right: -50%;
+        }
+      }
+
+      h4 {
+        color: #385185;
+        font-size: 14px;
+        font-weight: 500;
+        letter-spacing: 0.5px;
+        padding-top: 20px;
+      }
+
+      a {
+        color: rgb(66, 62, 62);
+        text-decoration: none;
+        font-size: 12px;
+        line-height: 14px;
+        margin-top: 25px;
+        margin-bottom: 15px;
+      }
+    }
+    &__register {
+      background: #fff;
+      height: 65px;
+      padding-top: 12px;
+      width: 75%;
+      p {
+        color: rgb(66, 62, 62);
+        font-weight: 300;
+        font-size: 14px;
+      }
+      a {
+        font-weight: 500;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
